1dfa53973057f89bafd8b2df460c7d9e115155c7
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2021-05-06 09:21:46 -0700

    beautify itn retention plot

--- intervention_impact/visualize_results/alt_01_calculate_intervention_impact.R
+++ intervention_impact/visualize_results/alt_01_calculate_intervention_impact.R
@@ -50,45 +50,94 @@ control <- final[ITN_Coverage==0, list(Site_Name, Run_Number, x_Temporary_Larval
 int <- final[ITN_Coverage>0]
 compare <- merge(control, int)
 
+control[, x_Temporary_Larval_Habitat:= round(x_Temporary_Larval_Habitat, 4)]
+ggplot(control[Site_Name<11 & Site_Name>1], aes(x=day, y=control_inc, color=factor(x_Temporary_Larval_Habitat))) +
+      geom_point() + 
+      geom_smooth() + 
+      facet_grid(Site_Name~ x_Temporary_Larval_Habitat)+
+      theme(legend.position = "none")
+
 compare[, x_Temporary_Larval_Habitat:= round(x_Temporary_Larval_Habitat, 4)]
 
-compare_summary <- compare[, lapply(.SD, mean), by = .(Site_Name, day, ITN_Coverage, ITN_Retention_Halflife, ITN_Initial_Block, ITN_Initial_Kill, x_Temporary_Larval_Habitat), 
+# assign intervention ids 
+ints <- unique(compare[, list(ITN_Coverage, ITN_Retention_Halflife, ITN_Blocking_Halflife, ITN_Initial_Kill, ITN_Initial_Block, ITN_Start)])[order(ITN_Coverage, ITN_Retention_Halflife, ITN_Blocking_Halflife, ITN_Initial_Kill, ITN_Initial_Block, ITN_Start)]
+ints[, int_id:= as.integer(rownames(ints))]
+compare <- merge(compare, ints, by=c("ITN_Coverage", "ITN_Retention_Halflife", "ITN_Blocking_Halflife", "ITN_Initial_Kill", "ITN_Initial_Block", "ITN_Start"))
+
+compare_summary <- compare[, lapply(.SD, mean), by = .(Site_Name, day, int_id, ITN_Coverage, ITN_Retention_Halflife, ITN_Initial_Block, ITN_Initial_Kill, x_Temporary_Larval_Habitat), 
                            .SDcols = c("control_inc", "control_severe_inc", "final_inc", "final_severe_inc")]
 
-day_to_plot <- 365
-sites_to_plot <-  c(3, 4, 10)
-kill_to_plot <- c(0.2)
+
+get_smooth <- function(x, y){
+  if (max(y)<0.05){
+    return(y)
+  }else{
+    lo <- loess(y[y>0]~x[y>0])
+    predictions <- c(y[y==0], predict(lo))
+    return(pmax(predictions, rep(0, length(predictions))))
+  }
+}
+
+smooth_compare <- lapply(unique(compare_summary$Site_Name), function(site_name){
+  sub_list <- lapply(unique(compare_summary$int_id), function(int_name){
+    sub_sub_list <- lapply(unique(compare_summary$day), function(this_day){
+      subset <- compare_summary[Site_Name==site_name & int_id==int_name & day==this_day]
+      # subset[, smooth_min:= get_smooth(mean_initial, min_final)]
+      # subset[, smooth_max:= get_smooth(mean_initial, max_final)]
+      subset[, smooth_mean:= get_smooth(control_inc, final_inc)]
+      return(subset)
+    })
+    sub_sub_list <- rbindlist(sub_sub_list)
+  })
+  sub_list <- rbindlist(sub_list)
+})
+smooth_compare <- rbindlist(smooth_compare)
+
+day_to_plot <- unique(compare$day)  # 365
+sites_to_plot <-  c(4)
+kill_to_plot <- c(0.4)
 ret_to_plot <- unique(compare$ITN_Retention_Halflife) # c(730)
-block_to_plot <- c(0.6, 0.9)
-x_temps_to_plot <- c(0.1995, 0.5012, 5.0119)
+block_to_plot <- c(0.3)
+cov_to_plot <- unique(compare$ITN_Coverage)
+x_temps_to_plot <- c(0.1259, 0.5012, 5.0119)
 
-subset <- compare[day==day_to_plot & Site_Name %in% sites_to_plot & ITN_Initial_Kill %in% kill_to_plot & ITN_Retention_Halflife %in% ret_to_plot & ITN_Initial_Block %in% block_to_plot]
-subset_summary <- compare_summary[day==day_to_plot & Site_Name %in% sites_to_plot & ITN_Initial_Kill %in% kill_to_plot & ITN_Retention_Halflife %in% ret_to_plot & ITN_Initial_Block %in% block_to_plot] 
+subset <- compare[day %in% day_to_plot & Site_Name %in% sites_to_plot & ITN_Initial_Kill %in% kill_to_plot & ITN_Retention_Halflife %in% ret_to_plot & ITN_Initial_Block %in% block_to_plot & ITN_Coverage %in% cov_to_plot]
+subset_summary <- smooth_compare[day %in% day_to_plot & Site_Name %in% sites_to_plot & ITN_Initial_Kill %in% kill_to_plot & ITN_Retention_Halflife %in% ret_to_plot & ITN_Initial_Block %in% block_to_plot & ITN_Coverage %in% cov_to_plot]
+
+subset_summary[, pct_change:= 100*(smooth_mean-control_inc)/control_inc]
+subset_summary[, pct_reduction:= -pct_change]
+
+subset_for_bars <- subset_summary[x_Temporary_Larval_Habitat %in% x_temps_to_plot]
+subset_for_bars[, transmission:= factor(x_Temporary_Larval_Habitat, levels=x_temps_to_plot, labels=c("Low", "Medium", "High"))]
 
 
-line_plots <- ggplot(subset, aes(x=control_inc, y=final_inc, color=factor(ITN_Retention_Halflife))) + 
-                  geom_vline(data=unique(subset_summary[x_Temporary_Larval_Habitat %in% x_temps_to_plot, 
-                                                        list(Site_Name, x_Temporary_Larval_Habitat, control_inc)]), 
+ggplot(subset_for_bars, aes(x=transmission, y=pct_reduction, fill=factor(ITN_Retention_Halflife))) +
+  geom_bar(stat="identity", position = "dodge", color="black") +
+  scale_fill_brewer(type="seq", palette = "YlGnBu", name="Retention\nHalf-Life\n(Days)") + 
+  theme_minimal() + 
+  facet_grid(ITN_Coverage ~ day) +
+  labs(x="Transmission Level",
+       y="% Reduction in Incidence",
+       title="Reduction in Incidence by Transmission Intensity, ITN Retention Half-Life, Time in Days (columns), and ITN Coverage (rows)")
+
+
+
+line_plots <- ggplot(subset_summary, aes(x=control_inc, color=factor(ITN_Retention_Halflife))) + 
+                  geom_vline(data=unique(subset_summary[x_Temporary_Larval_Habitat %in% x_temps_to_plot & Site_Name %in% sites_to_plot,
+                                                        list(Site_Name, day, x_Temporary_Larval_Habitat, control_inc)]),
                              aes(xintercept=control_inc)) +
                   geom_abline() + 
-                  geom_point(alpha=0.25) +
-                  geom_smooth(se=F, span=0.3) + 
-                  facet_grid(ITN_Coverage ~ Site_Name)
+                  geom_point(aes(y=final_inc), alpha=0.5) +
+                  scale_color_brewer(type="seq", palette = "YlGnBu", name="Retention\nHalf-Life") + 
+                  # geom_smooth(se=F, span=0.3) + 
+                  geom_line(aes(y=smooth_mean))+ 
+                  facet_grid(ITN_Coverage ~ day)
 
 
 # ggplot(control[x_Temporary_Larval_Habitat<10], aes(x=x_Temporary_Larval_Habitat, y=control_inc)) + 
 #   geom_point() +
 #   facet_wrap(~Site_Name)
 
-
-subset_summary[, pct_change:= 100*(final_inc-control_inc)/control_inc]
-subset_summary <- subset_summary[x_Temporary_Larval_Habitat %in% x_temps_to_plot]
-subset_summary[, transmission:= factor(x_Temporary_Larval_Habitat, levels=x_temps_to_plot, labels=c("Low", "Medium", "High"))]
-
-
-ggplot(subset_summary, aes(x=transmission, y=pct_change, fill=factor(ITN_Retention_Halflife))) +
-  geom_bar(stat="identity", position = "dodge", alpha=0.75) +
-  facet_grid(ITN_Coverage ~ Site_Name)
 
 
 
